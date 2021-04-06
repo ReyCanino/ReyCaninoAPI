e6804da6bc1dfb7ef8dea822617df7dba8c720ff
mariahv9 <mafehv1999@hotmail.com>
2021-04-05 19:17:58 -0500

    Correccion

--- src/main/java/edu/escuelaing/reycanino/api/APIController.java
+++ src/main/java/edu/escuelaing/reycanino/api/APIController.java
@@ -81,13 +81,8 @@ public class APIController {
 
     @GetMapping(value = "/consultar/{id}")
     public ResponseEntity<Horario> consultarReserva(@PathVariable() String id) {
-        try {
-            Horario horario = services.consultarReserva(id);
-            return new ResponseEntity<>(horario, HttpStatus.OK);
-        } catch (Exception e) {
-            Logger.getLogger(APIController.class.getName()).log(Level.SEVERE, null, e);
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        Horario horario = services.consultarReserva(id);
+        return new ResponseEntity<>(horario, HttpStatus.OK);
     }
 
     @GetMapping(value = "/horario/{id}")
